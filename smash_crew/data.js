137cdee7ced84041f6bc441c552c01ea4c95b4e5
bunnycou <noahjcou@gmail.com>
2024-09-15 07:57:38 -0400

    smash support

---
+++ smash_crew/data.js
@@ -0,0 +1,80 @@
+function loadVars() {
+    var redTeam = localStorage.getItem("redTeam")
+    var blueTeam = localStorage.getItem("blueTeam")
+    var redScore = localStorage.getItem("redScore")
+    var blueScore = localStorage.getItem("blueScore")
+    var redStocks = localStorage.getItem("redStocks")
+    var blueStocks = localStorage.getItem("blueStocks")
+
+    if (!redTeam) { redTeam = "red" }
+    if (!blueTeam) { blueTeam = "blue" }
+    if (!redScore) { redScore = "0" }
+    if (!blueScore) { blueScore = "0" }
+    if (!redStocks) { redStocks = "12" }
+    if (!blueStocks) { blueStocks = "12" }
+
+    document.getElementById("redTeam").value = redTeam
+    document.getElementById("blueTeam").value = blueTeam
+    document.getElementById("redScore").value = redScore
+    document.getElementById("blueScore").value = blueScore
+    document.getElementById("redStocks").value = redStocks
+    document.getElementById("blueStocks").value = blueStocks
+}
+
+function storeVars() {
+    let redTeam = document.getElementById("redTeam").value
+    let blueTeam = document.getElementById("blueTeam").value
+    let redScore = document.getElementById("redScore").value
+    let blueScore = document.getElementById("blueScore").value
+    let redStocks = document.getElementById("redStocks").value
+    let blueStocks = document.getElementById("blueStocks").value
+
+    localStorage.setItem("redTeam", redTeam)
+    localStorage.setItem("blueTeam", blueTeam)
+    localStorage.setItem("redScore", redScore)
+    localStorage.setItem("blueScore", blueScore)
+    localStorage.setItem("redStocks", redStocks)
+    localStorage.setItem("blueStocks", blueStocks)
+    localStorage.setItem("redPlayers", Math.ceil(redStocks/3))
+    localStorage.setItem("bluePlayers", Math.ceil(blueStocks/3))
+    localStorage.setItem("game", parseInt(blueScore)+parseInt(redScore)+1)
+    
+    loadVars();
+}
+
+function swapVars() {
+    let redTeam = document.getElementById("redTeam").value
+    let blueTeam = document.getElementById("blueTeam").value
+    let redScore = document.getElementById("redScore").value
+    let blueScore = document.getElementById("blueScore").value
+    let redStocks = document.getElementById("redStocks").value
+    let blueStocks = document.getElementById("blueStocks").value
+
+    var tmp
+    tmp = redTeam
+    redTeam = blueTeam
+    blueTeam = tmp
+
+    tmp = redScore
+    redScore = blueScore
+    blueScore = tmp
+
+    tmp = redStocks
+    redStocks = blueStocks
+    blueStocks = tmp
+
+    localStorage.setItem("redTeam", redTeam)
+    localStorage.setItem("blueTeam", blueTeam)
+    localStorage.setItem("redScore", redScore)
+    localStorage.setItem("blueScore", blueScore)
+    localStorage.setItem("redStocks", redStocks)
+    localStorage.setItem("blueStocks", blueStocks)
+    localStorage.setItem("game", parseInt(blueScore)+parseInt(redScore)+1)
+    
+    loadVars();
+}
+
+document.getElementById("submit").addEventListener("click", storeVars);
+document.getElementById("swap").addEventListener("click", swapVars);
+
+loadVars();
---
+++ smash_crew/game.js
@@ -0,0 +1,41 @@
+function load() {
+    var redTeam = localStorage.getItem("redTeam")
+    var blueTeam = localStorage.getItem("blueTeam")
+    var redScore = localStorage.getItem("redScore")
+    var blueScore = localStorage.getItem("blueScore")
+    var redStocks = localStorage.getItem("redStocks")
+    var blueStocks = localStorage.getItem("blueStocks")
+    var redPlayers = parseInt(localStorage.getItem("redPlayers"))
+    var bluePlayers = parseInt(localStorage.getItem("bluePlayers"))
+
+    if (!redTeam) { redTeam = "red" }
+    if (!blueTeam) { blueTeam = "blue" }
+    if (!redScore) { redScore = 0 }
+    if (!blueScore) { blueScore = 0 }
+    if (!redStocks) { redStocks = 12 }
+    if (!blueStocks) { blueStocks = 12 }
+
+    document.getElementById("redTeam").textContent = redTeam
+    document.getElementById("blueTeam").textContent = blueTeam
+    document.getElementById("redScore").textContent = redScore
+    document.getElementById("blueScore").textContent = blueScore
+    document.getElementById("redStocks").textContent = redStocks
+    document.getElementById("blueStocks").textContent = blueStocks
+    document.getElementById("redPlayers").textContent = playerString(redPlayers)
+    document.getElementById("bluePlayers").textContent = playerString(bluePlayers)
+}
+
+setInterval(load, 1000)
+
+function playerString(players) {
+    var ret = ""
+    for (var i = 0; i < 4; i++) {
+        if (i < players) {
+            ret += "\u25cf"
+        } else {
+            ret += "\u26ac"
+        }
+    }
+
+    return ret
+}
